41315356bbb9807758c0ec62ace19c7dc0eae4c7
bloombar <2209098+bloombar@users.noreply.github.com>
2020-02-08 21:36:38 -0500

    some comments

--- 1.basic_language.js
+++ 1.basic_language.js
@@ -19,7 +19,7 @@ const myFavoriteLanguage = "Javascript"; // etc, etc...
 // non-constant variables are declared with the keyword let
 let it = true; // it's true!
 it = (it) ? false : true; // reassign it to be its opposite using the ternary operator
-console.log(`It is ${it}!`);
+console.log(`It is ${it}!`); // expected output: 'It is true!'
 
 // for loop
 for (let i=1; i<6; i++) {
@@ -33,28 +33,28 @@ while (i < 6) {
     i++;
 }
 
-// an object (no classes necessary)
+// an object (no classes necessary - JavaScript uses prototypal inheritance instead)
 const me = {
     name: "Foo Barstein",
     phone: "212-666-1212",
     age: 63,
     isRobot: false
 };
-console.log(`My name is ${me.name}.`); // output "My name is Foo Barstein"
+console.log(`My name is ${me.name}.`); // expected output: "My name is Foo Barstein"
 
 // make a copy of an object
-const robotMe = Object.create(me); // creates a new object based on the original
-robotMe.isRobot = true; // change a property
-console.log(`me ${me.isRobot ? 'is indeed' : 'is not'} a robot.`); // output "My name is Foo Barstein"
-console.log(`robotMe ${robotMe.isRobot ? 'is indeed' : 'is not'} a robot.`); // output "My name is Foo Barstein"
+const robotMe = Object.create(me); // creates a new object based on the original prototype
+robotMe.isRobot = true; // modify this new object in some way
+console.log(`me ${me.isRobot ? 'is indeed' : 'is not'} a robot.`); // expected output: "My name is Foo Barstein"
+console.log(`robotMe ${robotMe.isRobot ? 'is indeed' : 'is not'} a robot.`); // expected output: "My name is Foo Barstein"
 
 // an object with a method belonging to it written in shorthand syntax
 const fido = {
     name: "Fido",
     breed: "Schnauzer",
     bark(message) {
-        console.log(`${this.name} the ${this.breed} says ${message}.`);
+        console.log(`${this.name} the ${this.breed} says, '${message}'`);
     }
 }
-fido.bark('Woof!');
+fido.bark('Woof!'); //expected output: "Fido the Schnauzer says, 'Woof!'"
 
